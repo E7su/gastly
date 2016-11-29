947e1d3e7c6a25792529931cf8e0db55155332ca
Etsu <etsu4296@gmail.com>
2016-11-29 14:35:26 +0300

    code optimization

--- max_pairwise_product.py
+++ max_pairwise_product.py
@@ -5,11 +5,10 @@ n = int(input())
 a = [int(x) for x in input().split()]
 assert (len(a) == n)
 
-result = 0
+max_1 = max(a)
+a.remove(max(a))
+max_2 = max(a)
 
-for i in range(0, n):
-    for j in range(i + 1, n):
-        if a[i] * a[j] > result:
-            result = a[i] * a[j]
+result = max_1 * max_2
 
 print(result)
