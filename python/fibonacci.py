40a279df587a0a285aaeb9639aca6eea607ba3fd
Etsu <etsu4296@gmail.com>
2017-01-09 21:39:02 +0300

    code optimization

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -2,8 +2,15 @@ def calc_fib(n):
     if (n <= 1):
         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    f.insert(0, 0)
+    f.insert(1, 1)
+
+    for i in range(2, n + 1):
+        f.insert(i, f[i - 1] + f[i - 2])
+    return f[i]
 
 
+f = list()
+# n = int(input('Input n: '))
 n = int(input())
 print(calc_fib(n))
