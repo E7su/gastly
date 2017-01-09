eaf2c5d0273321aee78c841720afed1f813fc8d9
Etsu <etsu4296@gmail.com>
2017-01-09 23:07:15 +0300

    code optimization

--- python/fibonacci_last_digit.py
+++ python/fibonacci_last_digit.py
@@ -1,18 +1,20 @@
 import sys
 
-def get_fibonacci_last_digit_naive(n):
+def get_fibonacci_last_digit_efficient(n):
     if n <= 1:
         return n
 
     previous = 0
-    current  = 1
+    current = 1
 
     for _ in range(n - 1):
         previous, current = current, previous + current
+        current = current % 10
+    return current
 
-    return current % 10
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    # n = int(input())
+    print(get_fibonacci_last_digit_efficient(n))
