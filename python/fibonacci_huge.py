50f473207c70b4b8bc352f5e6d75501738288f9e
Etsu <etsu4296@gmail.com>
2017-01-10 14:51:15 +0300

    code optimization fib_huge

--- python/fibonacci_huge.py
+++ python/fibonacci_huge.py
@@ -1,21 +1,30 @@
 # Uses python3
-import sys
+from itertools import count
 
 
-def get_fibonacci_huge_naive(n, m):
+def get_fibonacci_huge_efficient(n, m):
+    return fibonacci_mod(n % pisano_period(m), m)
+
+
+def pisano_period(m):
+    if m == 1:
+        return 1
+    prev, cur = 0, 1
+    for n in count(2):
+        prev, cur = cur, (prev + cur) % m
+        if (prev, cur) == (0, 1):
+            return n - 1
+
+
+def fibonacci_mod(n, m):
     if n <= 1:
         return n
-
-    previous = 0
-    current = 1
-
+    prev, cur = 0, 1
     for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % m
+        prev, cur = cur % m, (prev + cur) % m
+    return cur
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+    n, m = map(int, input().split())
+    print(get_fibonacci_huge_efficient(n, m))
