bb0485b2fdd57d54f48c79d2c0ebab64b4ee3ad4
Etsu <etsu4296@gmail.com>
2017-01-10 12:26:43 +0300

    rename function
    EuclidGCD() -> Euclid_GCD()

--- python/gcd.py
+++ python/gcd.py
@@ -1,15 +1,15 @@
 import sys
 
 
-def EuclidGCD(a, b):
+def Euclid_GCD(a, b):
     if b == 0:
         return a
 
     a = a % b
-    return EuclidGCD(b, a)
+    return Euclid_GCD(b, a)
 
 
 if __name__ == "__main__":
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(EuclidGCD(a, b))
+    print(Euclid_GCD(a, b))
