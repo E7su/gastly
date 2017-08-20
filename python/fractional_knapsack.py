ac651aa515e2b7aa7382a69cdc80ee9049173358
Etsu <etsu4296@gmail.com>
2017-08-20 11:22:48 +0300

    Create fractional_knapsack.py

---
+++ python/fractional_knapsack.py
@@ -0,0 +1,53 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+import sys
+
+"""Вычисление оптимальной стоимости вещей в рюкзаке
+
+   Arguments:
+   capacity - вместимость рюкзака (кг)
+   values_and_weights - лист с парами (стоимость, вес)
+
+   Return:
+   acc - стоимость рюкзака
+"""
+
+
+def fractional_knapsack(capacity, values_and_weights):
+    # value - стоимость предмета
+    # weight - вес предмета
+    # value / weight - удельная стоимость предмета
+    order = [(value / weight, weight) for value, weight in values_and_weights]
+    order.sort(reverse=True)
+
+    # acc - стоимость рюкзака на данный момент
+    acc = 0
+    for value_per_weight, weight in order:
+        # если предмет целиком убирается в рюкзак
+        if weight < capacity:
+            acc += value_per_weight * weight
+            capacity -= weight
+        else:
+            # если не умещается, то кладём сколько влезет
+            acc += value_per_weight * capacity
+            break
+
+    return acc
+
+
+def main():
+    reader = (tuple(map(int, line.split())) for line in sys.stdin)
+    # quantity - количество элементов, capacity - вместимость рюкзака
+    quantity, capacity = next(reader)
+    # пары - стоимость и вес
+    values_and_weights = list(reader)
+    # проверка что количество пар равно количеству элементов
+    assert len(values_and_weights) == quantity
+    # вычисение максимальной стоимость рюкзака
+    optimal_value = fractional_knapsack(capacity, values_and_weights)
+    # точность до трёх знаков после запятой
+    print("{:.3f}".format(optimal_value))
+
+
+if __name__ == "__main__":
+    main()
