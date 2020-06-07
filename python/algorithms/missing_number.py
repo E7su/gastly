1caf266fa5bb845eae4272513be88fd5810b8606
|K07e7su <etsu4296@gmail.com>
2020-06-07 19:45:06 +0300

    Create missing_number.py

---
+++ python/algorithms/missing_number.py
@@ -0,0 +1,11 @@
+# class Solution:
+#     def missingNumber(self, nums):
+#         missing = len(nums)
+#         for i, num in enumerate(nums):
+#             missing ^= i ^ num
+#         return missing
+class Solution:
+    def missingNumber(self, nums):
+        expected_sum = len(nums) * (len(nums) + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
