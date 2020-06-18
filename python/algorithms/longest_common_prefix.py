dda32453075273277190ac3c08dc98942a94fe15
|K07e7su <etsu4296@gmail.com>
2020-06-18 21:41:16 +0300

    Update longest_common_prefix.py

--- python/algorithms/longest_common_prefix.py
+++ python/algorithms/longest_common_prefix.py
@@ -3,11 +3,12 @@ class Solution(object):
         """
         :type strs: List[str]; rtype: str
         """
+        # [('f', 'f', 'f'), ('l', 'l', 'l'), ('o', 'o', 'i'), ('w', 'w', 'g')]
         symbols_strings = zip(*strs)
+        print(list(symbols_strings))
         ret = ""
-        # looping corrected based on @StefanPochmann's comment below
         for symbols in symbols_strings:
-            if len(set(symbols)) > 1:
+            if len(set(symbols)) > 1:  # ('o', 'o', 'i')
                 break
             ret += symbols[0]
         return ret
