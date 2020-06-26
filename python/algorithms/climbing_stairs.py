99a85e52f59211ef2f56fa99b454d43da299993c
K07e7su <etsu4296@gmail.com>
2020-06-26 22:18:50 +0300

    Create climbing_stairs.py

---
+++ python/algorithms/climbing_stairs.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3:
+            return n
+        result = 0
+        k = 1
+        if n % 2 == 0:
+            result += 1   # for 22... combination
+        for i in range(n - 1, n//2, -1):
+            result = int(result + self.get_c(i, k))   # for other combinations
+            k += 1
+        return result + 1   # for 1111... combination
+
+    def get_c(self, n, k):
+        c = math.factorial(n)/(math.factorial(n-k) * math.factorial(k))
+        return c
