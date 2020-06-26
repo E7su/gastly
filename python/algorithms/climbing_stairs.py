890122df3b6393636ba0d4246946af9db8205a3c
K07e7su <etsu4296@gmail.com>
2020-06-26 22:29:51 +0300

    Update climbing_stairs.py

--- python/algorithms/climbing_stairs.py
+++ python/algorithms/climbing_stairs.py
@@ -9,10 +9,10 @@ class Solution:
         k = 1
         if n % 2 == 0:
             result += 1   # for 22... combination
-        for i in range(n - 1, n//2, -1):
-            result = int(result + self.get_c(i, k))   # for other combinations
+        for i in range(n - 1, n // 2, -1):
+            result += self.get_c(i, k)  # for other combinations
             k += 1
-        return result + 1   # for 1111... combination
+        return int(result + 1)   # for 1111... combination
 
     def get_c(self, n, k):
         c = math.factorial(n)/(math.factorial(n-k) * math.factorial(k))
